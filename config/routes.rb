86bf34bbdc3deacdea16ea803cf5125e41aa4811
David Wilkie <dwilkie@gmail.com>
2017-03-17 17:46:11 +0700

    Redirect homepage to Github repo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root :to => redirect('https://github.com/dwilkie/dockerhub2ci')
+
   namespace :api, :defaults => { :format => 'json' } do
     resources :webhooks, :only => [:create, :show]
   end
