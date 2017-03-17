a6526e9bc1f29aae39303611af3e0214fa333e78
David Wilkie <dwilkie@gmail.com>
2017-03-17 12:21:08 +0700

    Add support for HTTP Basic authentication

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,5 +1,7 @@
 class Api::BaseController < ApplicationController
   protect_from_forgery :with => :null_session
+  before_action :authorize!
+
   respond_to :json
 
   def create
@@ -16,6 +18,19 @@ class Api::BaseController < ApplicationController
   end
 
   private
+
+  def api_keys
+    @api_keys ||= ENV["API_KEYS"].to_s.split(";")
+  end
+
+  def authorize!
+    deny_access! if !ActionController::HttpAuthentication::Basic.has_basic_credentials?(request)
+    authenticate_with_http_basic { |api_key| !api_keys.include?(api_key) }
+  end
+
+  def deny_access!
+    head(:unauthorized)
+  end
 
   def build_resource
     @resource ||= association_chain.new(permitted_params)
--- spec/requests/api/webhooks_spec.rb
+++ spec/requests/api/webhooks_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
 describe "Webhooks API" do
+  include EnvHelpers
+
+  let(:api_key) { "SOME-API-KEY" }
+  let(:request_headers) { {} }
+  let(:headers) { authorization_headers.merge(request_headers) }
+
+  def env
+    @env ||= {
+      :api_keys => api_key
+    }
+  end
+
+  def authorization_headers
+    @authorization_headers ||= {
+      "HTTP_AUTHORIZATION" => ActionController::HttpAuthentication::Basic.encode_credentials(api_key, "")
+    }
+  end
+
   def setup_scenario
+    stub_env(env)
     do_request
   end
 
@@ -14,25 +33,29 @@ describe "Webhooks API" do
   end
 
   describe "POST '/api/webhooks'" do
-    include EnvHelpers
-
     let(:asserted_tag) { "latest" }
     let(:payload) { attributes_for(:webhook, :tag => asserted_tag)[:payload] }
+    let(:request_headers) { { "CONTENT_TYPE" => "application/json" } }
 
     def do_request
       post(
         api_webhooks_path,
         :params => payload.to_json,
-        :headers => {"CONTENT_TYPE" => "application/json"}
+        :headers => headers
       )
+    end
+
+    context "unauthorized request" do
+      let(:authorization_headers) { {} }
+      let(:asserted_response_code) { "401" }
+      it { assert_response! }
     end
 
     context "webhooks are configured to be saved" do
       let(:asserted_response_code) { "201" }
 
-      def setup_scenario
-        stub_env(:save_webhooks => "1")
-        super
+      def env
+        super.merge(:save_webhooks => "1")
       end
 
       def assert_response!
@@ -55,9 +78,16 @@ describe "Webhooks API" do
     let(:asserted_response_code) { "200" }
 
     def do_request
-      get(api_webhook_path(webhook))
+      get(api_webhook_path(webhook), :headers => headers)
     end
 
     it { assert_response! }
+
+    context "unauthorized request" do
+      let(:authorization_headers) { {} }
+      let(:asserted_response_code) { "401" }
+
+      it { assert_response! }
+    end
   end
 end
